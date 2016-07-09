b6751678792c4d4730d148737829be9fd0b347c2
tkstanczak <t.k.stanczak@gmail.com>
2016-07-09 15:37:51 +0100

    splendor.html

--- splendor.py
+++ splendor.py
@@ -1,12 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+def splendor(name = None):
+    return render_template('splendor.html', name=name)
 
 if __name__ == '__main__':
     app.run()
