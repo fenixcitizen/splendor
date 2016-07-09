1b2dae47ee0aa35cb3989fe66f4dcdbc966042db
tkstanczak <t.k.stanczak@gmail.com>
2016-07-09 19:08:21 +0100

    request attempt

--- splendor.py
+++ splendor.py
@@ -6,10 +6,30 @@ import os
 app = Flask(__name__)
 
 all_cards = []
+first_row_cards = []
+
+
+def initialize_game():
+    global all_cards
+    base_dir = os.path.dirname(__file__)
+    lookup_dir = os.path.join(base_dir, 'static/cards')
+    all_cards = list(listdir_nohidden(lookup_dir))
+    initialize_first_row()
+
+
+def initialize_first_row():
+    card1 = random.choice(all_cards)
+    card2 = random.choice(all_cards)
+    card3 = random.choice(all_cards)
+    card4 = random.choice(all_cards)
+    global first_row_cards
+    first_row_cards = [card1, card2, card3, card4]
+
 
 @app.route('/')
-def splendor():
-    return display_cards_at_random()
+def display_game():
+    return render_template('splendor.html', cards=first_row_cards)
+
 
 def listdir_nohidden(path):
     for f in os.listdir(path):
@@ -17,25 +37,15 @@ def listdir_nohidden(path):
             yield f
 
 
-def display_cards_at_random():
-    card1 = random.choice(all_cards)
-    card2 = random.choice(all_cards)
-    card3 = random.choice(all_cards)
-    card4 = random.choice(all_cards)
-    return render_template('splendor.html', cards=[card1, card2, card3, card4])
-
-
 @app.route('/choose_card', methods=['GET', 'POST'])
 def choose_card():
-    return display_cards_at_random()
+    global first_row_cards
+    for card in first_row_cards
+        if card == request.data
+    first_row_cards[0] = random.choice(all_cards)
+    return display_game()
 
-
-def load_all_cards():
-    global all_cards
-    base_dir = os.path.dirname(__file__)
-    lookup_dir = os.path.join(base_dir, 'static/cards')
-    all_cards = list(listdir_nohidden(lookup_dir))
 
 if __name__ == '__main__':
-    load_all_cards()
+    initialize_game()
     app.run()
