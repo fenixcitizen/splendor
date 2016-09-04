e95402843f3926cd7ab60a903fdf5767cc25b97d
tkstanczak <t.k.stanczak@gmail.com>
2016-09-04 22:14:04 +0100

    game exception tests

--- game/tests/test_game.py
+++ game/tests/test_game.py
@@ -7,19 +7,31 @@ from game.Game import Player
 class TestGame(unittest.TestCase):
 
     def setUp(self):
-        self._player1 = Player()
-        self._player2 = Player()
+        self._player1 = Player('Player1')
+        self._player2 = Player('Player2')
         self._players = [self._player1, self._player2]
         self._game_under_test = Game(self._players)
 
     def __init__(self):
         pass
 
+    def test_throws_when_number_of_players_is_below_two(self):
+        one_player = [self._player1]
+        self.assertRaises(GameException, Game(one_player))
+
+    def test_throws_when_number_of_players_is_below_two(self):
+        five_players = [self._player1, self._player2, Player('Player3'), Player('Player4'), Player('Player5')]
+        self.assertRaises(GameException, Game(five_players))
+
+    def test_throws_when_players_contains_same_player_twice(self):
+        duplicated_players = [self._player1, self._player2, self._player2]
+        self.assertRaises(GameException, Game(duplicated_players))
+
     def test_initialized_with_correct_current_player(self):
         self.assertEqual(self._game_under_test.current_player, self._player1)
 
-    def test_initialized_with_correct_number_of_players(self):
-        self.assertEqual(len(self._game_under_test.players), len(self._players))
+    def test_initialized_with_correct_players(self):
+        self.assertListEqual(self._game_under_test.players, self._players)
 
     def test_initialized_with_first_turn(self):
         self.assertEqual(self._game_under_test.is_last_turn, False)
